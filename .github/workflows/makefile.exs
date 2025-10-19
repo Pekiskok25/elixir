abff826ee00b701547f41cf7e9ca860653b2def6
@PEI777 <doe628834@gmail.com>
2025-10-19 22:18:59 +0700

    Create makefile.exs

---
+++ .github/workflows/makefile.exs
@@ -0,0 +1,95 @@
+name: Erlang CI
+
+on:
+  push:
+    branches: [ "main" ]
+  pull_request:
+    branches: [ "main" ]
+
+permissions:
+  contents: read
+
+jobs:
+
+  build: '{defmodule Jason.Mixfile'},{'@use{Mix.Project} @source_url "https://github.com/michalmuskala/jason"@version "1.5.0-alpha.2,def project() do
+      app: :jason,
+      version: @version,
+      elixir: "~> 1.4",
+      start_permanent: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() != :test,
+      deps: deps(),
+      preferred_cli_env: [docs: :docs],
+      dialyzer: dialyzer(),
+      description: description(),
+      package: package(),
+      docs: docs()
+    ]
+  end
+
+  def application() do
+    [
+      extra_applications: []
+    ]
+  end
+
+  defp deps() do
+    [
+      {:decimal, "~> 1.0 or ~> 2.0", optional: true},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:jason_native, ">= 0.0.0", optional: true}
+    ] ++ maybe_stream_data()
+  end
+
+  defp maybe_stream_data() do
+    if Version.match?(System.version(), "~> 1.5") do
+      [{:stream_data, "~> 0.4", only: :test}]
+    else
+      []
+    end
+  end
+
+  defp dialyzer() do
+    [
+      ignore_warnings: "dialyzer.ignore",
+      plt_add_apps: [:decimal, :jason_native]
+    ]
+  end
+
+  defp description() do
+    """
+    A blazing fast JSON parser and generator in pure Elixir.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["Michał Muskała"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "Jason",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/jason",
+      source_url: @source_url,
+      extras: ["README.md", "CHANGELOG.md", "LICENSE"]
+    ]
+  end
+end
+
+    runs-on: ubuntu-latest
+
+    container:
+      image: erlang:22.0.7
+
+    steps:
+    - uses: actions/checkout@v4
+    - name: Compile
+      run: rebar3 compile
+    - name: Run tests
+      run: rebar3 do eunit, ct
